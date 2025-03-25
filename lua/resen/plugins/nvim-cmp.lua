9a73e9b048426de47943cf507358fff73cbef131
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2025-03-25 15:51:18 -0600

    feat: remove adding parenthesis when autocompleting a function

--- lua/resen/plugins/nvim-cmp.lua
+++ lua/resen/plugins/nvim-cmp.lua
@@ -18,8 +18,8 @@ return {
 		local select_opts = { behavior = cmp.SelectBehavior.Select }
 
 		-- If you want insert `(` after select function or method item
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 		cmp.setup({
 			formatting = {
