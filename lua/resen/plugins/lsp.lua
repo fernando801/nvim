84c85d2e6de78df175ef85d3427b32181edbc600
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2025-03-30 11:15:02 -0600

    feat: use telescope to find lsp_definitions

--- lua/resen/plugins/lsp.lua
+++ lua/resen/plugins/lsp.lua
@@ -15,7 +15,6 @@ return {
 				local opts = { buffer = bufnr, remap = false }
 
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 				vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
