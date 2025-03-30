30e8f654c15719c1d5ebd1692bfbbf9be9694644
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2025-03-30 00:04:26 -0600

    feat: add .luarc.json

--- lua/resen/plugins/lsp.lua
+++ lua/resen/plugins/lsp.lua
@@ -47,11 +47,6 @@ return {
 			},
 			handlers = {
 				lsp_zero.default_setup,
-				lua_ls = function()
-					-- (Optional) Configure lua language server for neovim
-					local lua_opts = lsp_zero.nvim_lua_ls()
-					require("lspconfig").lua_ls.setup(lua_opts)
-				end,
 			},
 		})
 	end,
