aa461cdd24b749db00aebc0f8cbcec8f25050d81
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2025-03-30 00:32:59 -0600

    feat: remove lsp-zero from lspconfig

--- lua/resen/plugins/lsp.lua
+++ lua/resen/plugins/lsp.lua
@@ -6,37 +6,38 @@ return {
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
 		"williamboman/mason-lspconfig.nvim",
-		"VonHeikemen/lsp-zero.nvim",
 	},
 	config = function()
-		-- This is where all the LSP shenanigans will live
-		local lsp_zero = require("lsp-zero")
-		lsp_zero.extend_lspconfig()
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(ev)
+				local bufnr = ev.buf
 
-		--- if you want to know more about lsp-zero and mason.nvim
-		--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-		lsp_zero.on_attach(function(client, bufnr)
-			-- see :help lsp-zero-keybindings
-			-- to learn the available actions
-			-- lsp_zero.default_keymaps({ buffer = bufnr })
+				local opts = { buffer = bufnr, remap = false }
 
-			local opts = { buffer = bufnr, remap = false }
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+				vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+				vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+				vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+				vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>rnm", vim.lsp.buf.rename, opts)
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-			vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-			vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
-			vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
-			vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-			vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-			vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-			vim.keymap.set("n", "<leader>rnm", vim.lsp.buf.rename, opts)
-		end)
+				-- Enable neovim autocompletion
+				--
+				-- local client = vim.lsp.get_client_by_id(ev.data.client_id)
+				--
+				-- if client:supports_method("textDocument/completion") then
+				-- 	vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+				-- end
+			end,
+		})
 
 		require("mason-lspconfig").setup({
 			ensure_installed = {
@@ -45,8 +46,15 @@ return {
 				"lua_ls",
 				"rust_analyzer",
 			},
+			-- See `:h mason-lspconfig.setup_handlers()`
+			---@type table<string, fun(server_name: string)>?
 			handlers = {
-				lsp_zero.default_setup,
+				-- The first entry (without a key) will be the default handler
+				-- and will be called for each installed server that doesn't have
+				-- a dedicated handler.
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({})
+				end,
 			},
 		})
 	end,
