8797116db77393eafccf51d9012b0cf97a147cce
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2025-03-30 12:41:46 -0600

    feat: remove telescope file browser extension

--- lua/resen/plugins/telescope.lua
+++ lua/resen/plugins/telescope.lua
@@ -4,30 +4,21 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope-ui-select.nvim",
-		"nvim-telescope/telescope-file-browser.nvim",
 	},
 	config = function()
 		require("telescope").setup()
-		require("telescope").load_extension("file_browser")
 		local builtin = require("telescope.builtin")
 
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { noremap = true })
 		vim.keymap.set("n", "<leader>gf", builtin.git_files, { noremap = true })
 		vim.keymap.set("n", "<leader>rg", builtin.live_grep, { noremap = true })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true })
+		vim.keymap.set("n", "<leader>gh", builtin.help_tags, { noremap = true })
+		vim.keymap.set("n", "<leader>gb", builtin.buffers, { noremap = true })
 		vim.keymap.set("n", "gd", builtin.lsp_definitions, { noremap = true })
 		vim.keymap.set("n", "gr", builtin.lsp_references, { noremap = true })
-		-- vim.keymap.set("n", "<leader>bf", builtin.buffers, { noremap = true })
-		vim.keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<CR>", { noremap = true })
 		-- Find hidden and ignored files
-		vim.keymap.set(
-			"n",
-			"<leader>hf",
-			"<cmd>Telescope find_files find_command=rg,--no-ignore,--hidden,--files<CR>",
-			{ noremap = true }
-		)
-		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
+		vim.keymap.set("n", "<leader>hf", function()
+			builtin.find_files({ hidden = true, no_ignore = true })
+		end, { noremap = true })
 	end,
 }
