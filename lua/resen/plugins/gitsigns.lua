08e11cd444c635fcd5945b1b6044591bd5237045
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2025-04-21 11:58:45 -0600

    feat: add remaps for git hunks

--- lua/resen/plugins/gitsigns.lua
+++ lua/resen/plugins/gitsigns.lua
@@ -4,7 +4,25 @@ return {
 	config = function()
 		local gitsigns = require("gitsigns")
 		gitsigns.setup()
-		vim.keymap.set("n", "<leader>tb", gitsigns.toggle_current_line_blame, { noremap = true })
-		vim.keymap.set("n", "<leader>dt", gitsigns.diffthis, { noremap = true })
+		-- Toggle blame information for the current line
+		vim.keymap.set(
+			"n",
+			"<leader>tb",
+			gitsigns.toggle_current_line_blame,
+			{ noremap = true, desc = "Toggle Git blame for the current line" }
+		)
+		-- Show diff for the current file
+		vim.keymap.set(
+			"n",
+			"<leader>dt",
+			gitsigns.diffthis,
+			{ noremap = true, desc = "View Git diff of the current file" }
+		)
+		-- Preview the contents of the current hunk
+		vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { noremap = true, desc = "Preview Git hunk" })
+		-- Navigate to the previous hunk
+		vim.keymap.set("n", "[c", gitsigns.prev_hunk, { noremap = true, desc = "Go to previous Git hunk" })
+		-- Navigate to the next hunk
+		vim.keymap.set("n", "]c", gitsigns.next_hunk, { noremap = true, desc = "Go to next Git hunk" })
 	end,
 }
