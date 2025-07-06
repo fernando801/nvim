98993b6a3b7981292171d8917ab8ef8232e31554
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2025-07-05 19:06:51 -0600

    feat(ftplugin): change indent depending of the location of the project

--- after/ftplugin/javascriptreact.lua
+++ after/ftplugin/javascriptreact.lua
@@ -1,4 +1,12 @@
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+local file_path = vim.fn.expand("%:p")
+local work_dir = vim.fn.expand("$WORK_DIR")
+
+local indent = 2
+if string.find(file_path, work_dir, 1, true) == 1 then
+	indent = 4
+end
+
+vim.opt.tabstop = indent
+vim.opt.softtabstop = indent
+vim.opt.shiftwidth = indent
 vim.opt.wrap = true
