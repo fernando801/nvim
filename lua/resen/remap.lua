1fc776a88f39c8114831ac35c9c4539b919526f2
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2026-01-19 13:38:25 -0600

    feat: copy to clipboard only on yanking

--- lua/resen/remap.lua
+++ lua/resen/remap.lua
@@ -25,3 +25,11 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "y", '"+y', { noremap = true, silent = true }) -- y{motion}
+vim.keymap.set("n", "yy", '"+yy', { noremap = true, silent = true }) -- yy
+vim.keymap.set("n", "Y", '"+Y', { noremap = true, silent = true }) -- Y (yank to end of line)
+
+-- Visual-mode yanks
+vim.keymap.set("x", "y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("x", "Y", '"+Y', { noremap = true, silent = true })
