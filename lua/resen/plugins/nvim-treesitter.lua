b619697acfa7b4c7568ae6ad50593b0e7434f14a
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2026-05-09 10:48:37 -0600

    fix(treesitter): remove deprecated dependency

--- lua/resen/plugins/nvim-treesitter.lua
+++ lua/resen/plugins/nvim-treesitter.lua
@@ -3,92 +3,89 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	dependencies = {
-		"nvim-treesitter/playground",
 		"nvim-treesitter/nvim-treesitter-context",
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			modules = {},
-			-- A list of parser names, or "all"
-			ensure_installed = {
-				"vim",
-				"vimdoc",
-				"html",
-				"css",
-				"javascript",
-				"typescript",
-				"tsx",
-				"python",
-				"go",
-				"cpp",
-				"c",
-				"lua",
-				"rust",
-				"astro",
-				"markdown",
-				"markdown_inline",
-				"sql",
-			},
+	config = {
+		modules = {},
+		-- A list of parser names, or "all"
+		ensure_installed = {
+			"vim",
+			"vimdoc",
+			"html",
+			"css",
+			"javascript",
+			"typescript",
+			"tsx",
+			"python",
+			"go",
+			"cpp",
+			"c",
+			"lua",
+			"rust",
+			"astro",
+			"markdown",
+			"markdown_inline",
+			"sql",
+		},
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = false,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
+		-- Automatically install missing parsers when entering buffer
+		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+		auto_install = true,
 
-			-- List of parsers to ignore installing (for "all")
-			ignore_install = {},
+		-- List of parsers to ignore installing (for "all")
+		ignore_install = {},
 
-			---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-			-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+		---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+		-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-			highlight = {
-				-- `false` will disable the whole extension
+		highlight = {
+			-- `false` will disable the whole extension
+			enable = true,
+
+			-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+			-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+			-- the name of the parser)
+			-- list of language that will be disabled
+			--disable = { "c", "rust" },
+			-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+			--disable = function(lang, buf)
+			--    local max_filesize = 100 * 1024 -- 100 KB
+			--    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			--    if ok and stats and stats.size > max_filesize then
+			--        return true
+			--    end
+			--end,
+
+			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+			-- Using this option may slow down your editor, and you may see some duplicate highlights.
+			-- Instead of true it can also be a list of languages
+			additional_vim_regex_highlighting = false,
+		},
+
+		-- For more information about textobjects configurations read the docs at https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+		textobjects = {
+			select = {
 				enable = true,
-
-				-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-				-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-				-- the name of the parser)
-				-- list of language that will be disabled
-				--disable = { "c", "rust" },
-				-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-				--disable = function(lang, buf)
-				--    local max_filesize = 100 * 1024 -- 100 KB
-				--    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				--    if ok and stats and stats.size > max_filesize then
-				--        return true
-				--    end
-				--end,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-
-			-- For more information about textobjects configurations read the docs at https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-			textobjects = {
-				select = {
-					enable = true,
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
-						["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
-						["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
-						["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-					},
-					include_surrounding_whitespace = true,
+				-- Automatically jump forward to textobj, similar to targets.vim
+				lookahead = true,
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["af"] = { query = "@function.outer", desc = "Select outer part of a function" },
+					["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
+					["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
+					["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
 				},
+				include_surrounding_whitespace = true,
 			},
+		},
 
-			indent = {
-				enable = true,
-			},
-		})
-	end,
+		indent = {
+			enable = true,
+		},
+	},
 }
