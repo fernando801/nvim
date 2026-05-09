5b344c271a868250f35e8eae76742b4cc96103b8
Fernando Resendiz <38899746+fernando801@users.noreply.github.com>
2026-05-09 11:44:39 -0600

    fix: fix jdtls

--- lua/resen/plugins/nvim-jdtls.lua
+++ lua/resen/plugins/nvim-jdtls.lua
@@ -5,20 +5,32 @@ return {
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "java",
 			callback = function()
-				local root_dir = require("jdtls.setup").find_root({ ".git", "pom.xml", "mvnw", "gradlew" })
+				-- Anchor JDT LS at the repo/build root so Maven reactor projects import once.
+				local root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" })
 				if not root_dir then
 					return
 				end
 
 				local project_name = vim.fn.fnamemodify(root_dir, ":t")
 				local workspace_dir = "/Users/resen/.jdtls-workspace-root/" .. project_name
+				local java_home = "/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home"
+				local mason_jdtls_dir = "/Users/resen/.local/share/nvim/mason/packages/jdtls"
+				local lombok_jar = mason_jdtls_dir .. "/lombok.jar"
+				local java_settings = {
+					eclipse = {
+						downloadSources = true,
+					},
+					maven = {
+						downloadSources = true,
+					},
+				}
 				local config = {
 					-- The command that starts the language server
 					-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
 					cmd = {
 
 						-- 💀
-						"/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home/bin/java", -- or '/path/to/java17_or_newer/bin/java'
+						java_home .. "/bin/java", -- or '/path/to/java21_or_newer/bin/java'
 						-- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
 						"-Declipse.application=org.eclipse.jdt.ls.core.id1",
@@ -27,6 +39,7 @@ return {
 						"-Dlog.protocol=true",
 						"-Dlog.level=ALL",
 						"-Xmx1g",
+						"-javaagent:" .. lombok_jar,
 						"--add-modules=ALL-SYSTEM",
 						"--add-opens",
 						"java.base/java.util=ALL-UNNAMED",
@@ -62,7 +75,7 @@ return {
 					-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
 					-- for a list of options
 					settings = {
-						java = {},
+						java = java_settings,
 					},
 
 					-- Language server `initializationOptions`
@@ -74,6 +87,9 @@ return {
 					-- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
 					init_options = {
 						bundles = {},
+						settings = {
+							java = java_settings,
+						},
 					},
 				}
 				-- This starts a new client & server,
